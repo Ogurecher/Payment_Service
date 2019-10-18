5e8831ec6f8cad8ff50986e045190904e2279f9a
Dmitrii <1@1.1>
2019-10-18 16:33:28 +0300

    Added Swagger (not sure if it's working)

--- src/main/java/swagger/SwaggerApplication.java
+++ src/main/java/swagger/SwaggerApplication.java
@@ -1,25 +1,48 @@
-/*import static spark.Spark.*;
+package swagger;
 
 import com.google.gson.Gson;
 import dto.UserDetailsDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.jaxrs.config.BeanConfig;
 import service.PaymentService;
 import util.CustomLogger;
 
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Api (value = "Payment Service", description = "Payment Service API")
-public class Main {
+import static spark.Spark.port;
+import static spark.Spark.put;
+
+@Api(value = "Payment Service", description = "Payment Service API")
+public class SwaggerApplication extends Application {
+
+    public SwaggerApplication() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setBasePath("/api");
+        beanConfig.setResourcePackage("org.jazzteam");
+        beanConfig.setScan(true);
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet();
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return resources;
+    }
+
     private final static Logger logger = Logger.getLogger(CustomLogger.class.getName());
     private static PaymentService paymentService = new PaymentService();
 
     public static void main(String[] args) {
         CustomLogger.init();
-
         port(1810);
 
         performPayment();
@@ -37,4 +60,5 @@ public class Main {
             );
         });
     }
-}*/
+}
+
